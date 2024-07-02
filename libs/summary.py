ae42fba35af30a82650daf1886a15a19ca56f282
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2024-07-02 09:03:26 +0200

    I layer corrispondono agli ulsp-types. Riorganizzazione codice senza umap-template.json

--- libs/summary.py
+++ libs/summary.py
@@ -38,119 +38,297 @@ La pagina dedicata nel sito Web del progetto è [[{WebPageURL}|qui]]
 
 summary_filename = "summary.umap"
 summary_URL = "https://umap.openstreetmap.fr/it/map/sommario_1044830"
+
+# Inizializzazione umap mappa Summary 
+
 summary = {
-    "type": "umap",
-    "geometry": {
-        "type": "Point",
-        "coordinates": [
-            10.405556,
-            44.121944
-        ]
+	"type": "umap",
+	"geometry": {
+    "type": "Point",
+    "coordinates": [
+      10.405556,
+      44.121944
+    ]
+  },
+	"properties": {
+    "name": "Sommario",
+    "zoom": 11,
+    "easing": False,
+    "licence": "",
+    "miniMap": False,
+    "overlay": {},
+    "facetKey": "ulsp_type,Tag primario,Dataset",
+    "slideshow": {},
+    "tilelayer": {
+       "tms": False,
+       "name": "OSM OpenTopoMap",
+       "maxZoom": 20,
+       "minZoom": 1,
+       "attribution": "Kartendaten: © [[https://openstreetmap.org/copyright|OpenStreetMap]]-Mitwirkende, [[http://viewfinderpanoramas.org/|SRTM]] | Kartendarstellung: © [[https://opentopomap.org/|OpenTopoMap]]    ([[https://creativecommons.org/licenses/by-sa/3.0/|CC-BY-SA]])",
+       "url_template": "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
     },
-    "properties": {
-        "name": "Sommario",
-        "zoom": 11,
-        "easing": False,
-        "licence": "",
-        "miniMap": False,
-#        "overlay": null,,
-        "facetKey": "ulsp_type,Tag primario,Dataset",
-        "slideshow": {
-
-        },
-        "tilelayer": {
-          "tms": False,
-          "name": "OSM OpenTopoMap",
-          "maxZoom": 20,
-          "minZoom": 1,
-          "attribution": "Kartendaten: © [[https://openstreetmap.org/copyright|OpenStreetMap]]-Mitwirkende, [[http://viewfinderpanoramas.org/|SRTM]] | Kartendarstellung: © [[https://opentopomap.org/|OpenTopoMap]]    ([[https://creativecommons.org/licenses/by-sa/3.0/|CC-BY-SA]])",
-          "url_template": "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
-        },
-        "captionBar": False,
-        "description": "",
-        "limitBounds": {
-
-        },
-        "moreControl": True,
-        "zoomControl": True,
-        "captionMenus": True,
-        "embedControl": True,
-        "scaleControl": True,
-        "searchControl": True,
-        "scrollWheelZoom": True,
-        "datalayersControl": True,
-        "fullscreenControl": True,
-        "displayPopupFooter": False,
-        "permanentCreditBackground": True
+    "captionBar": False,
+    "description": "Summary map of Underlandscape activity and results",
+    "limitBounds": {},
+    "moreControl": True,
+    "zoomControl": True,
+    "captionMenus": True,
+    "embedControl": True,
+    "scaleControl": True,
+    "searchControl": True,
+    "scrollWheelZoom": True,
+    "datalayersControl": True,
+    "fullscreenControl": True,
+    "displayPopupFooter": False,
+    "permanentCreditBackground": True
+  },
+  "layers": [
+    {
+      "type": "FeatureCollection",
+      "features": [],
+      "_umap_options": {
+          "name": "Percorso",
+          "displayOnLoad": True,
+          "browsable": True,
+          "remoteData": {},
+          "popupShape": "Panel",
+          "popupTemplate": "Table",
+          "popupContentTemplate": '''\
+# {Titolo}
+##   ( [[https://sites.google.com/view/prin-underlandscape/home-page/attivit%C3%A0-sul-campo/{Link}|link]]) 
+##   [[{GitHubURL}|GitHub repository]] 
+{Descrizione}
+--- 
+**Lunghezza** (km): {Lunghezza}
+**Durata**: {Durata}
+**Dislivello in salita** (m): {Dislivello in salita}
+**Dislivello in discesa** (m): {Dislivello in discesa} 
+--- 
+Registrato il {Data} alle {Ora} 
+con {Strumento} 
+da {Autore}
+'''
+      }
     },
-    "layers": []
+    {
+      "type": "FeatureCollection",
+      "features": [ ],
+      "_umap_options": {
+        "name": "POI",
+        "displayOnLoad": True,
+        "browsable": True,
+        "remoteData": {},
+        "iconClass": "Ball",
+        "popupShape": "Panel",
+        "popupTemplate": "Table",
+        "popupContentTemplate": '''\
+# {Titolo}
+{{{Foto}|300}} 
+##   [[{GitHubURL}|GitHub repository]] 
+##   [[{Link}|Link]] alla pagina dedicata 
+{Descrizione} 
+**Tag primario**: {Tag primario}
+**Altri tag**: {Altri tag}
+**Altitudine** (m): {Altitudine} 
+--- 
+Scattata il {Data} alle {Ora} 
+con {Strumento} 
+da {Autore}
+'''
+      }
+    },
+    {
+      "type": "FeatureCollection",
+      "features": [ ],
+      "_umap_options": {
+          "name": "QRtag",
+          "displayOnLoad": True,
+          "browsable": True,
+          "remoteData": {},
+          "iconClass": "Ball",
+          "popupShape": "Panel",
+          "popupTemplate": "Table",
+          "popupContentTemplate": '''\
+# {Titolo} 
+{{{Foto}|300}} 
+## [[{GitHubURL}|GitHub repository]] 
+{Testo} 
+ 
+FID: {fid}
+'''
+      }
+    },
+    {
+      "type": "FeatureCollection",
+      "features": [ ],
+      "_umap_options": {
+          "name": "Sito",
+          "displayOnLoad": True,
+          "browsable": True,
+          "remoteData": {},
+          "iconClass": "Ball",
+          "popupShape": "Panel",
+          "popupTemplate": "Table",
+          "popupContentTemplate": '''\
+# SITO {Sito}
+{{{Foto}|300}}
+## {Titolo} 
+##   [[{GitHubURL}|GitHub repository]] 
+## [[{Link}|Link]] alla pagina dedicata
+{Descrizione}
+---
+**Tipologia**: {Tipologia sito}
+**Definizione**: {Definizione}
+**Cronologia iniziale**: {Cronologia iniziale}
+**Cronologia finale**: {Cronologia finale}
+**Reperti ceramici**: {Reperti ceramici}
+**Reperti geologici**: {Reperti geologici}
+**Reperti organici**: {Reperti organici}
+**Altri manufatti**: {Altri manufatti}
+---
+**Altitudine** (m): {Altitudine}
+**Sicurezza**: {Sicurezza}
+**Accessibilità**: {Accessibilità}
+**Copertura rete mobile**: {Copertura rete mobile}
+**Copertura GPS**: {Copertura GPS}
+---
+**Provincia**: {Provincia}
+**Comune**: {Comune}
+**Toponimo**: {Toponimo}
+**Microtoponimo**: {Microtoponimo}
+**Strade d'accesso**: {Strade d'accesso}
+**Altra localizzazione**: {Altri elementi di localizzazione}
+---
+**Prima visita**: {Data} {Ora}
+**Strumento**: {Strumento}
+[[{Bibliografia}|Link]] alla bibliografia
+'''
+      }
+    },
+    {
+      "type": "FeatureCollection",
+      "features": [ ],
+      "_umap_options": {
+        "name": "Risorsa",
+        "displayOnLoad": True,
+        "browsable": True,
+        "remoteData": {},
+        "iconClass": "Ball",
+        "popupShape": "Panel",
+        "popupTemplate": "Table",
+        "popupContentTemplate": '''\
+# {Titolo}
+{{{Foto}|300}}
+{Descrizione}
+**Altitudine** (m): {Altitudine}
+**Repository GitHub**: [[{GitHubURL}|link]]
+**Pagina descrittiva dell'itinerario**: [[{ULSPLink}|link]]
+**Pagina descrittiva della risorsa** :[[{Link}|link]]
+**Tag primario**: {Tag primario}
+**Altri tag**: {Altri tag}"
+'''
+      }
+    },
+    {
+      "type": "FeatureCollection",
+      "features": [ ],
+      "_umap_options": {
+        "name": "Itinerario",
+        "displayOnLoad": True,
+        "browsable": True,
+        "remoteData": {},
+        "popupShape": "Panel",
+        "popupTemplate": "Table",
+        "popupContentTemplate": '''\
+# {Titolo}
+## Itinerario {Tipologia}
+   ( [[https://sites.google.com/view/prin-underlandscape/home-page/attivit%C3%A0-sul-campo/{Link}|link]]) 
+##   [[{GitHubURL}| Download ]] 
+{Descrizione}
+--- 
+**Lunghezza** (km): {Lunghezza}
+**Durata**: {Durata}
+**Dislivello in salita** (m): {Dislivello in salita}
+**Dislivello in discesa** (m): {Dislivello in discesa}
+'''
+      }
+    }
+  ]
 }
 
+# Viene invocata nella directory ottenuta scaricando il repo Master
 def generate_summary(ul):
-  # Read configuration and assets
-  # with open("dataset-sync.config") as json_data:
-    # config = json.load(json_data)
-  # with open("umapTemplate.json") as json_data:
-    # umapTemplate = json.load(json_data)
-    
-  # logo = Image.open("logoEle_v2.2_small.png").convert("RGBA")
-  
-  # colprint.emphprint("Clono il repository Master da github")
-  # my_git.clone(config['masterName'])
-  # os.chdir(config['masterName'])
+
   emphprint("Generazione della mappa umap di sommario")
-  # filtra i file con estensione geojson
+	# filtra i file con estensione geojson
   geojson_files = list(
-                filter(lambda fn: os.path.splitext(fn)[1] == ".geojson", os.listdir(".")) 
-              )
-  #print(geojson_files)
-  
+    filter(lambda fn: os.path.splitext(fn)[1] == ".geojson", os.listdir(".")) 
+  )
+	#print(geojson_files)
+	
   for fn in geojson_files:
     emphprint("  Includo il dataset " + os.path.splitext(fn)[0])
     with open(fn) as json_data:
-        geojson = json.load(json_data)
+      geojson = json.load(json_data)
     random.seed(sum(map(ord,fn)))
-    color = hex(random.randint(0,16777215)).replace('0x','#');
-    geojson["_umap_options"] = {
-      "name": os.path.splitext(fn)[0],
-      "editMode": "advanced",
-      "browsable": True,
-      "inCaption": True,
-      "remoteData": {},
-      "displayOnLoad": True,
-      "iconClass": "Drop",
-      "color": color,
-      "popupTemplate": "Default",
-      "popupContentTemplate": popupTemplate
-    }
-    
+    randomColor = hex(random.randint(0,16777215)).replace('0x','#')
+		
     for f in geojson['features']:
-#      print(f)
+      ulspType = f['properties']['ulsp_type']
       f['properties']['Dataset'] = os.path.splitext(fn)[0]
       f['properties']['Link GitHub'] = 'https://github.com/prin-underlandscape/'+os.path.splitext(fn)[0]
-  # Compatibilità quando mancano le properties del dataset (geojson)
-      if 'properties' in geojson:
+      try:
         f['properties']['umapURL'] = geojson['properties']['umapKey']
-        try:
-          f['properties']['WebPageURL'] = geojson['properties']['WebPageURL']
-        except KeyError as e:
-          print(f'{e=}')
-        f['properties']['name'] = f['properties']['Titolo']
-      if '_umap_options' not in f['properties']: f['properties']['_umap_options']={}
-      if f['properties']['ulsp_type'] == 'Sito':
-        f['properties']['_umap_options']['iconClass'] = 'Ball'
-        f['properties']['_umap_options']['color'] = '#800101' #Override color for sites
-      elif f['properties']['ulsp_type'] == 'POI':
-        f['properties']['_umap_options']['iconClass'] = 'Circle'
-      elif f['properties']['ulsp_type'] == 'QRtag':
-        f['properties']['_umap_options']['iconClass'] = 'Drop'
-        f['properties']['_umap_options']['iconUrl']= "/uploads/pictogram/guidepost.svg"
-  #  print(color)
-    summary['layers'].append(geojson)
-  
+        f['properties']['WebPageURL'] = geojson['properties']['WebPageURL']
+      except KeyError as e:
+        print(f'Error: {e}')
+      f['properties']['name'] = f['properties']['Titolo']
+      f['properties']['_umap_options']={}
+      if ulspType == 'Sito':
+        f['properties']['_umap_options'] = {
+          'iconClass': 'Ball',
+          'color': '#800101'
+        }
+      elif ulspType == 'POI':
+        f['properties']['_umap_options'] = {
+          'iconClass': 'Circle',
+          'color': randomColor
+        }
+      elif ulspType == 'Percorso':
+        f['properties']['_umap_options'] = {
+          "weight": 9,
+          'color': randomColor
+        }
+      elif ulspType == 'QRtag':
+        f['properties']['_umap_options'] = {
+          'iconClass': 'Drop',
+          'color': randomColor,
+          'iconUrl': "/uploads/pictogram/guidepost.svg"
+        }
+      elif ulspType == 'Risorsa':
+        f['properties']['_umap_options'] = {
+          'iconClass': 'Circle',
+          'color': randomColor
+        }
+      elif ulspType == 'Itinerario':
+        f['properties']['_umap_options'] = {
+          "weight": 9,
+          'color': randomColor
+        }
+      else:
+        raise KeyError("Tipo di feature non prevista")
+      try:
+        layer=next(
+          filter(lambda l: l["_umap_options"]["name"] == ulspType, summary["layers"])
+        )
+        layer["features"].append(f)
+        print(f'{ulspType} - {f["properties"]["name"]}')
+      except Exception as e:
+        print(f'Error: {e}')
+	
   with open("../summary.umap", 'w', encoding='utf-8') as f:  
     json.dump(summary, f , ensure_ascii=False, indent=2)
-  
+	
   ul.log(summary_filename,summary_URL)
-  
-  
+	
+	
